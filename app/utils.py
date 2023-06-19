f7c53c21199e51c633cf4f7e8920b194596b22e7
Breaddcrumbss <lukechong2898.lc@gmail.com>
2023-06-19 21:41:45 +0800

    env

--- app/utils.py
+++ app/utils.py
@@ -8,13 +8,13 @@ def classify(text):
     path = os.path.join(settings.MEDIA_ROOT, 'files/fitted_classifier.pkl')
     with open(path, 'rb') as file:
         classifier = pickle.load(file)
-        predicted_labels = classifier.predict([text])[0]
+        predicted_labels = classifier.predict([text.lower()])[0]
     
     return predicted_labels
 
 def find_cases(label):
     df = pd.read_csv(os.path.join(settings.MEDIA_ROOT, 'files/Name-URL-Catchword.tsv'), sep='\t')
-    matches = df[df['Simple Catchwords'] == label][['Name', 'URL', 'Case Text']].head(3)
+    matches = df[df['Simple Catchwords'] == label][['Name', 'URL', 'Case Text']].head(10)
     cases = []
     for index, row in matches.iterrows():
         case_info = {}
@@ -24,3 +24,4 @@ def find_cases(label):
         cases.append(case_info)
     
     return cases
+
